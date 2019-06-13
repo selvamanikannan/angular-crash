84c57716cd5974d94b851c5255d2ef327de0f301
selvamanikannan <selvamanikannan55@gmail.com>
2019-06-14 01:03:09 +0530

    new component for data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +10,15 @@ export class HomeComponent implements OnInit {
   
   h1Style: boolean = false
 
-  constructor() { }
+  constructor(private data : DataService) { }
 
   ngOnInit() {
   }
 
   firstClick(){
-    this.h1Style = true;
-
-    console.log("Clicked");
+    // this.h1Style = true;
+    this.data.firstClick();
+    // console.log("Clicked");
   }
 
 }
